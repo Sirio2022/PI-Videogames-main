34c56dae3cc2de08df22b2be5debdbafa737eecf
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-10-10 18:48:28 -0500

    reducer ahi HP

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,7 +1,17 @@
-import { GET_ALLVIDEOGAMES } from "../actions";
+import {
+  GET_ALLVIDEOGAMES,
+  GET_GENRES,
+  GET_PLATFORMS,
+  GET_VIDEOGAMES_BY_NAME,
+  GET_VIDEOGAME_BY_ID,
+} from "../actions";
 
 const initialstate = {
   videogames: [],
+  videogamesfilter: [],
+  videogamedetails: [],
+  genres: [],
+  platforms: [],
 };
 
 export default function rootreducer(state = initialstate, action) {
@@ -11,13 +21,71 @@ export default function rootreducer(state = initialstate, action) {
         return {
           ...state,
           videogames: action.payload,
+          videogamesfilter: action.payload,
         };
-      }else{
-        return{
-            ...state,
-            videogames: []
-        }
+      } else {
+        return {
+          ...state,
+          videogames: [],
+        };
       }
+    case GET_VIDEOGAMES_BY_NAME:
+      return {
+        ...state,
+        videogames: action.payload,
+      };
+    case GET_VIDEOGAME_BY_ID:
+      return {
+        ...state,
+        videogamedetails: action.payload,
+      };
+    case GET_GENRES:
+      let genre = action.payload;
+      genre.unshift("All");
+      return {
+        ...state,
+        genres: genre,
+      };
+    case GET_PLATFORMS:
+      return{
+        ...state,
+        platforms: action.payload
+      }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     default:
       return state;
   }
